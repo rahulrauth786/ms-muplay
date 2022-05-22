d096f555a7a077d9fe3565731849483a193a3591
rahulrauth786 <83859768+rahulrauth786@users.noreply.github.com>
2022-05-22 20:47:15 +0530

    Add files via upload

---
+++ settings/local.js
@@ -0,0 +1,8 @@
+module.exports = {
+  APP_POSTGRESQL_CONNECTION:
+    "postgres://postgres:rahul1234@postgres-instance-1.c5qktbbunfgq.ap-south-1.rds.amazonaws.com:5432/postgres",
+  MASTER_POSTGRESQL_CONNECTION:
+    "postgres://postgres:rahul1234@postgres-instance-1.c5qktbbunfgq.ap-south-1.rds.amazonaws.com:5432/postgres",
+  APP_POSTGRESQL_READ_REPLICA:
+    "postgres://postgres:rahul1234@postgres-instance-1.c5qktbbunfgq.ap-south-1.rds.amazonaws.com:5432/postgres",
+};
---
+++ settings/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+  APP_POSTGRESQL_CONNECTION: "postgres://postgres:1234@localhost:5020/postgres",
+  MASTER_POSTGRESQL_CONNECTION:
+    "postgres://postgres:1234@localhost:5020/postgres",
+  APP_POSTGRESQL_READ_REPLICA:
+    "postgres://postgres:1234@localhost:5020/postgres",
+};
